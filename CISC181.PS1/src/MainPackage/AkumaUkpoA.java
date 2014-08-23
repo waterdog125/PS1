8bfb9213506ab951ebfa80ea65b522e8b777d99d
johnn_000 <johnn_000@Akuma-XPS.home>
2014-08-23 16:19:48 -0400

    Added AkumaUkpoA

---
+++ CISC181.PS1/src/MainPackage/AkumaUkpoA.java
@@ -0,0 +1,9 @@
+package MainPackage;
+
+public class AkumaUkpoA {
+	
+	public static void HelloWorld(){
+	
+		System.out.println("Hello World");
+	}
+}
--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -5,10 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		/*
-		 * My Github Account Name: <Enter your Github Account name>
+		 * My Github Account Name: <akumaukpo>
 		 * 
 		 */
-		GibbonsB.HelloWorld();
+		AkumaUkpoA.HelloWorld();
 		
 	}
 

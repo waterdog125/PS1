8f65eb5e05c67c8b8e8ef18cf2235c517bb949c4
jbrosch <jbrosch@udel.edu>
2014-08-27 14:54:33 -0400

    added in class file and updated main

---
+++ CISC181.PS1/src/MainPackage/BroschJ.java
@@ -0,0 +1,10 @@
+package MainPackage;
+
+public class BroschJ {
+
+	public static void HelloWorld(){
+		
+		System.out.println("Hello World");
+	}
+	
+}
--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		StaffordP.Helloworld();
 		MatimuM.HelloWorld();
 		SalahuddinZ.HelloWorld();
+		BroschJ.HelloWorld();
 	}
 
 }

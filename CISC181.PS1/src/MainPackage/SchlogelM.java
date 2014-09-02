7c639a80c2ebd6604505039168b368c3dcfd372d
matthewschlogel <matthewschlogel@wifi-roaming-128-4-173-192.host.udel.edu>
2014-09-01 20:39:41 -0400

    Assignment Completed

--- CISC181.PS1/src/MainPackage/SchlogelM.java
+++ CISC181.PS1/src/MainPackage/SchlogelM.java
@@ -5,7 +5,7 @@ public class SchlogelM {
 	public static void HelloWorld(){
 	
 		System.out.println("Hello World");
-		//My GitHub account name: Mschlog
+		//My GitHub account name: Mschlog 
 	}
 }
 

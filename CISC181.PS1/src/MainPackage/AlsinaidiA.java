90ddf7d3241df51ddb3cae28c8b188aacc232375
LENOVO <LENOVO@128.4.206.242>
2014-09-04 00:44:20 +0400

    added my name Alsinaidiu

---
+++ CISC181.PS1/src/MainPackage/AlsinaidiA.java
@@ -0,0 +1,9 @@
+package MainPackage;
+
+public class AlsinaidiA {
+	
+	public static void HelloWorld(){
+	
+		System.out.println("Hello World");
+	}
+}
--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -41,6 +41,7 @@ public class Main {
 		McDanielsD.HelloWorld();
 		DehnC.HelloWorld();
 		DavisM.HelloWorld();
+		AlsinaidiA.HelloWorld();
 	}
 
 }

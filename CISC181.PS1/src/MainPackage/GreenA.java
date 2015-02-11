588178b420b2a46945180b9fbbc53615891efc0b
Zachary <Zachary@128.4.241.47>
2015-02-11 17:40:56 -0500

    commit

---
+++ CISC181.PS1/src/MainPackage/GreenA.java
@@ -0,0 +1,10 @@
+package MainPackage;
+//My Github Account Name: Andregreen
+
+public class GreenA {
+	public static void HelloWorld(){
+		
+		System.out.println("Hello World");
+	}
+
+}
--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		SellS.HelloWorld();
 		McParlandK.HelloWorld();
 		WagnerE.HelloWorld();
-		
+		GreenA.HelloWorld();
 		
 	}
 

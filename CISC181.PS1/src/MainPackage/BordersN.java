7c4f4ba4c6de93a7b4c8c2068812b39e512e028b
Nate <Nate@wifi-roaming-128-4-156-82.host.udel.edu>
2015-02-12 00:27:57 -0500

    Added BordersN

--- CISC181.PS1/src/MainPackage/BordersN.java
+++ CISC181.PS1/src/MainPackage/BordersN.java
@@ -1,28 +1,10 @@
 package MainPackage;
 public class BordersN {
+	//My Github Account Name: horsetornadox
 	public static void HelloWorld(){
 		
 		System.out.println("Hello World");
 	}
 	
-public static void main(String[] args) {
-		
-		GibbonsB.HelloWorld();
-		SellS.HelloWorld();
-		McParlandK.HelloWorld();
-		WagnerE.HelloWorld();
-		GreenA.HelloWorld();
-		SchreiberZ.HelloWorld();
-		RansomW.HelloWorld();
-		OrtizE.HelloWorld();
-		BakerM.HelloWorld();
-		YooJ.HelloWorld();
-		RinaldiT.HelloWorld();
-		ChinmayaA.HelloWorld();
-		QuinlanJ.HelloWorld();
-		LasherH.HelloWorld();
-		CarpenterH.HelloWorld();
-		BordersN.HelloWorld();
-	}
 
 }

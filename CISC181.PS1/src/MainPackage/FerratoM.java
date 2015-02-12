87423402f4dfd677e42e22aa7fc6ca0b8d0fe01a
mferrato <mferrato@udel.edu>
2015-02-12 15:23:27 -0500

    Added FerratoM

---
+++ CISC181.PS1/src/MainPackage/FerratoM.java
@@ -0,0 +1,11 @@
+package MainPackage;
+
+// My GitHub name is mferrato
+
+public class FerratoM {
+	public static void HelloWorld(){
+		
+		System.out.println("Hello World");
+	}
+
+}
--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -24,6 +24,7 @@ public class Main {
 		FurlongRobert.HelloWorld();
 		ShawR.HelloWorld();
 		BergerJason.HelloWorld();
+		FerratoM.HelloWorld();
 		
 	}
 
